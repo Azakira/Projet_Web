6edb830a4e15a6cbca43b7832c5a484823d4ef42
root <root@LAPTOP-6RRHP5A5.localdomain>
2019-10-01 23:20:51 +0200

    Extracted data from csv to a php array. TODO: Regroup dates in arrays

---
+++ jours.php
@@ -0,0 +1,13 @@
+<?php
+	echo "lalala";
+	/*
+	if (($handle = fopen("ResultatsFestival.csv", "r")) !== FALSE) {
+		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			foreach($data as $value) {
+				echo $data[$value] . "<br />\n";
+			}
+		}
+		fclose($handle);
+	} */
+	
+?>
