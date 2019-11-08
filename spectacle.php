9def7f54a9413d53457c5fa3066655960f377844
Tony <tony.abemonty@u-psud.fr>
2019-11-08 22:54:05 +0100

    spectacle sort by alphabetical order
    sort spectacles by alphabetical order

--- spectacle.php
+++ spectacle.php
@@ -6,11 +6,16 @@
 	</head>
 	
 	<body>
-		<div class="bandeau">						
+		<div class="bandeau">	
+			<div class="petitPanier">
+				<table>Billets en vente exclusivement sur les lieux du festival: Monétay,Monteignet, Veauce  du 2 au 6 août dès 11h00 et le 6 août à Moulins de 19h00 à 20h00.
+					Attention! à Moulins le début du spectacle à 20h00.
+				</table>
+			</div><!-- class="petitPanier"-->					
 			<h1> Festival Théâtres de Bourbon : Spectacle par spectacle </h1>
 		</div ><!--class="bandeau"-->
 		<div class="menu">
-			<ul class="navbar">
+			<ul class="nav">
 				<a href="index.php">Le site :</a>
 				<li>Qui sommes nous?</li>
 				<li><a href="jours.php">Jour par Jour</a></li>
@@ -18,155 +23,203 @@
 				<li><a href="spectacle.php">Spectacles</a></li>
 				<li>Tarifs</li>
 			</ul>			
+			<div class="riquiqui">
+				<ul class="nav">
+ 					<a href="#labelSpectacle_1">
+						 <li><titreSpectacle>Tartuffe ou l'imposteur<titreSpectacle></l>
+					</a>
+					<li>Les confessions : Les années de Jeunesse</li>
+					<li>Mademoiselle Julie</li>
+					<li>Le marchand de Venise</li>
+					<li>La gloire de mon père</li>
+					<li>Le château de ma mère</li>
+					<li>Les soliloques de Mariette</li>
+ 					<a href="Festival2018ProgrammationParSpectacle.php#labelSpectacle_8">
+						 <li><titreSpectacle> Barbara, où rêvent mes saisons<titreSpectacle></l>
+					</a>
+					<li>Le prophète</li>
+					<li>Le mariage</li>
+					<li>Les confessions : Les années de Maturité</li>
+					<li>Titre Provisoire</li>
+					<li> La demande en mariage et L'ours</li>
+					<li> Fricassée de berniques sur lit de Prévert</li>
+					<li>Les confessions : Les années de Sagesse</li>
+					<li>Hugo es tu là?</li>
+					<li>La promesse de l'aube</li>
+					<li>Port Racines</li>
+				</ul>
+			</div><!--class="riquiqui"-->
 		</div>
-		<main>
-			<div class="decalage">
-				<div class="Lieu">
-					<p>	
-					Dès cette première édition, le festival propose une programmation ambitieuse et riche. Les troupes participantes sont toutes aguerries et professionnelles et elles ont comme nous une approche passionnée du théâtre. 
-					Cet événement se veut novateur et ambitieux, puisque l'excellence des spectacles sera accessible à tous, dans une ambiance amicale et chaleureuse.
-				</p>
-					<h2> Notre programmation...</h2>
-					<h3> ...18 spectacles sélectionnés pour vous par notre directeur artistique.
-					</h3>
-					<p><li><titreSpectacle>Barbara, où rêvent mes saisons?</titreSpectacle>,
-							d’après 
-							<Auteur>Barbara et Sophie Pincemaille </Auteur>.	
-						</li>
-						<li><titreSpectacle>Le château de ma mère</titreSpectacle>,
-							d’après 
-							<Auteur>Marcel Pagnol</Auteur>.	
-						</li>
-						<li>
-							Un triptyque sur 3 jour tiré 
-							<titreSpectacle>des confessions</titreSpectacle>, 
-							de 
-							<Auteur>Saint Augustin</Auteur>.
-						</li>
-						<li>	
-							<titreSpectacle>La demande en mariage et l’ours</titreSpectacle>, 
-							de 
-							<Auteur>Tchekhov</Auteur>.	
-						</li> 
-						<li>	<titreSpectacle>Fricassée de Berniques sur lit de Prévert</titreSpectacle>, lectures choisies de 
-							<Auteur>Prévert</Auteur> parsemées de chansons.
-						</li> 	
-						<li><titreSpectacle>La gloire de mon père</titreSpectacle>, d’après 
-							<Auteur>Marcel Pagnol</Auteur>. 
-						</li>
-						<li><titreSpectacle>Hugo démasqué</titreSpectacle>, d’après 
-							<Auteur>Victor Hugo</Auteur>. 
-						</li>
-						<li><titreSpectacle>Mademoiselle Julie</titreSpectacle>, 
-							d'<Auteur>August Stringberg</Auteur>. 
-						</li>	
-						<li><titreSpectacle>Le Marchand de Venise</titreSpectacle>, 
-							de 
-							<Auteur>Shakespeare</Auteur>. 
-						</li>
-						<li><titreSpectacle>Le mariage</titreSpectacle>, 
-							de <Auteur>Jean Luc Jeener</Auteur>.
-						</li>
-						<li><titreSpectacle>La promesse de l'aube</titreSpectacle>, 
-							tirée de l'œuvre de 
-							<Auteur>Romain Gary</Auteur>. 
-						</li>
-						<li><titreSpectacle>Port racines</titreSpectacle>, 
-							création de 
-							<Auteur>labelle et Cie</Auteur>.
-						</li>
-						<li><titreSpectacle>Le prophète</titreSpectacle>, 
-							de 
-							<Auteur>Khalil Gilbran</Auteur>.
-						</li>
-						<li><titreSpectacle>Les soliloques de Mariette</titreSpectacle>, 
-							tirée de Belle du Seigneur, d'<Auteur>Albert Cohen</Auteur>.
-						</li>
-						<li><titreSpectacle>le Tartuffe ou l'imposteur</titreSpectacle>, 
-							de 
-							<Auteur>Molière</Auteur>. 
-						</li>
-						<li><titreSpectacle>titre Provisoire</titreSpectacle>, 
-							de 
-							<Auteur>Pauline Mornet</Auteur>, notre <a href="presentation.php#label_carteBlanche">carte blanche</a>. 
-						</li>
-						<h2> navigation au coeur des spectacles</h2>
-				
-					<p>
-					Dans la liste indiquant notre programmation un clic sur le titre du spectacle vous orientera directement vers son résumé et ses dates de représentations.
-					Réprésentations que vous pourrez alors choisir de mettre dans le panier pour y réserver ensuite des places (dès la réservation ouverte).
-					Quand un clic sur le nom de la compagnie vous redirigera vers son site web professionnel.
-					Vous pouvez aussi accèder directement au détail d'un spectacle en cliquant simplement sur son affiche miniature.
-					Pour revenir ici, il vous suffit de cliquer de nouveau sur l'onglet "spectacles" dans le menu du site.
-					Vous pouvez aussi dérouler la description de tous les spectacles dans l'ordre alphabetique de titre via l'asenceur de votre navigateur. 
+			<main>
+				<div class="decalage">
+					<div class="Lieu">
+						<p>	
+						Dès cette première édition, le festival propose une programmation ambitieuse et riche. Les troupes participantes sont toutes aguerries et professionnelles et elles ont comme nous une approche passionnée du théâtre. 
+						Cet événement se veut novateur et ambitieux, puisque l'excellence des spectacles sera accessible à tous, dans une ambiance amicale et chaleureuse.
 					</p>
-
-				</div>
-
-
-		<?php 
-			if (($handle = fopen("ResultatsFestival.csv", "r")) !== FALSE) {
-				fgetcsv($handle, 1000, ",");//On retire la 1ere ligne du csv (legendes)
-				$spectacle = "null";
-				$tab = array();
-				echo "<br/>\n";
-				while (($data = fgetcsv($handle, 1000, "\n")) !== FALSE) {
+						<h2> Notre programmation...</h2>
+						<h3> ...18 spectacles sélectionnés pour vous par notre directeur artistique.
+						</h3>
+						<p><li><titreSpectacle>Barbara, où rêvent mes saisons?</titreSpectacle>,
+								d’après 
+								<Auteur>Barbara et Sophie Pincemaille </Auteur>.	
+							</li>
+							<li><titreSpectacle>Le château de ma mère</titreSpectacle>,
+								d’après 
+								<Auteur>Marcel Pagnol</Auteur>.	
+							</li>
+							<li>
+								Un triptyque sur 3 jour tiré 
+								<titreSpectacle>des confessions</titreSpectacle>, 
+								de 
+								<Auteur>Saint Augustin</Auteur>.
+							</li>
+							<li>	
+								<titreSpectacle>La demande en mariage et l’ours</titreSpectacle>, 
+								de 
+								<Auteur>Tchekhov</Auteur>.	
+							</li> 
+							<li>	<titreSpectacle>Fricassée de Berniques sur lit de Prévert</titreSpectacle>, lectures choisies de 
+								<Auteur>Prévert</Auteur> parsemées de chansons.
+							</li> 	
+							<li><titreSpectacle>La gloire de mon père</titreSpectacle>, d’après 
+								<Auteur>Marcel Pagnol</Auteur>. 
+							</li>
+							<li><titreSpectacle>Hugo démasqué</titreSpectacle>, d’après 
+								<Auteur>Victor Hugo</Auteur>. 
+							</li>
+							<li><titreSpectacle>Mademoiselle Julie</titreSpectacle>, 
+								d'<Auteur>August Stringberg</Auteur>. 
+							</li>	
+							<li><titreSpectacle>Le Marchand de Venise</titreSpectacle>, 
+								de 
+								<Auteur>Shakespeare</Auteur>. 
+							</li>
+							<li><titreSpectacle>Le mariage</titreSpectacle>, 
+								de <Auteur>Jean Luc Jeener</Auteur>.
+							</li>
+							<li><titreSpectacle>La promesse de l'aube</titreSpectacle>, 
+								tirée de l'œuvre de 
+								<Auteur>Romain Gary</Auteur>. 
+							</li>
+							<li><titreSpectacle>Port racines</titreSpectacle>, 
+								création de 
+								<Auteur>labelle et Cie</Auteur>.
+							</li>
+							<li><titreSpectacle>Le prophète</titreSpectacle>, 
+								de 
+								<Auteur>Khalil Gilbran</Auteur>.
+							</li>
+							<li><titreSpectacle>Les soliloques de Mariette</titreSpectacle>, 
+								tirée de Belle du Seigneur, d'<Auteur>Albert Cohen</Auteur>.
+							</li>
+							<li><titreSpectacle>le Tartuffe ou l'imposteur</titreSpectacle>, 
+								de 
+								<Auteur>Molière</Auteur>. 
+							</li>
+							<li><titreSpectacle>titre Provisoire</titreSpectacle>, 
+								de 
+								<Auteur>Pauline Mornet</Auteur>, notre <a href="presentation.php#label_carteBlanche">carte blanche</a>. 
+							</li>
+							<h2> navigation au coeur des spectacles</h2>
 					
-					foreach($data as $value) {
-						$replaced = preg_replace_callback(
-							'/"(\\\\[\\\\"]|[^\\\\"])*"/',
-							function ($match){
-								$temp = preg_replace("[,]", '&#44;', $match);
-								implode($temp);
-								return $temp[0];
-							},
-							$value
-						);
-						$fields = preg_split("[,]", $replaced);
-						array_push($tab, $fields);
-					}
-				}
+						<p>
+						Dans la liste indiquant notre programmation un clic sur le titre du spectacle vous orientera directement vers son résumé et ses dates de représentations.
+						Réprésentations que vous pourrez alors choisir de mettre dans le panier pour y réserver ensuite des places (dès la réservation ouverte).
+						Quand un clic sur le nom de la compagnie vous redirigera vers son site web professionnel.
+						Vous pouvez aussi accèder directement au détail d'un spectacle en cliquant simplement sur son affiche miniature.
+						Pour revenir ici, il vous suffit de cliquer de nouveau sur l'onglet "spectacles" dans le menu du site.
+						Vous pouvez aussi dérouler la description de tous les spectacles dans l'ordre alphabetique de titre via l'asenceur de votre navigateur. 
+						</p>
 
-				$sort_spec = array();	//Pour y mettre le tableau trié en fonction des jours
-				$last_spec = "null";
-				while(count($tab)>0){ 
-					foreach($tab as $line){
-						//parcourir le tableau pour chercher le nouveau $last_lieu
-						if($last_spec!= $line[2]){
-							$last_spec = $line[2];
-							break 1;
+					</div>
+
+			<?php 
+				if (($handle = fopen("ResultatsFestival.csv", "r")) !== FALSE) {
+					fgetcsv($handle, 1000, ",");//On retire la 1ere ligne du csv (legendes)
+					$spectacle = "null";
+					$tab = array();
+					while (($data = fgetcsv($handle, 1000, "\n")) !== FALSE) {
+						
+						foreach($data as $value) {
+							$replaced = preg_replace_callback(
+								'/"(\\\\[\\\\"]|[^\\\\"])*"/',
+								function ($match){
+									$temp = preg_replace("[,]", '&#44;', $match);
+									implode($temp);
+									return $temp[0];
+								},
+								$value
+							);
+							$fields = preg_split("[,]", $replaced);
+							array_push($tab, $fields);
 						}
 					}
-					foreach($tab as $i => $line){
-						if($tab[$i][2] == $last_spec){
-							array_push($sort_spec, $tab[$i]);
-							unset($tab[$i]);
+
+					$sort_spec = array();	//Pour y mettre le tableau trié en fonction des spectacles
+					$last_spec = "null";
+					$spec_aplh = " ";
+					while(count($tab)>0){ 
+						foreach($tab as $line){
+							//parcourir le tableau pour chercher le nouveau $last_spec
+
+							if($last_spec!= $line[2]){
+								$last_spec = $line[2];
+								break 1;
+							}
+						}
+						$tab_asso = array();
+						foreach($tab as $i => $line){
+							if($tab[$i][2] == $last_spec){	
+								array_push($tab_asso, $tab[$i]);
+								unset($tab[$i]);
+							}	
+						}
+						$sort_spec[$last_spec] = $tab_asso;
+					}  //tant que $tab et $sort_lieu ont un nbre d'elts !=	
+						
+					ksort($sort_spec);
+
+
+					foreach($sort_spec as $key => $value){		//Boucle pour l'affichage html
+						foreach($value as $key2 => $value2){
+						
+							if($spectacle != $key){
+								if($spectacle != "null"){
+									echo "</table>\n";
+									echo "</div><!--class=\"Spectacle\"-->\n";
+									
+								}
+								$spectacle = $key;
+								echo "<div class=\"Spectacle\">\n";
+								echo "<titrespectacle id=\"labelSpectacle_1\"><h2> " . $spectacle . "</h2></titrespectacle>\n";
+							// 	switch ($line) {
+							// 		case "Tartuffe ou l'imposteur":
+							// 				echo "<figure id=\"TitreSpectacle\">\n<img scr=\"1.jpg\">";
+							// 				break;
+							// 		case "Les confessions : Les années de Jeunesse":
+							// 				echo "<figure id=\"spectacle\">\n<img scr=\"2.jpg\">";
+							// 				break;
+							// 		case " Mademoiselle Julie":
+							// 				echo "<figure id=\"spectacle\">\n<img scr=\"3.jpg\">";
+							// 				break;
+							// }
+								echo "<table>\n";
+								
+							}
+						
+						echo "<tr>\n<td>";
+						echo "<Horaire> Le " . $value2[0] . " " . " à " . $value2[1] . "</Horaire> " .  "par <troupe>" . $value2[5] . "</troupe><br/>\n";
+						echo "</td>\n <td>Reserver</td></tr>";
 						}
 					}
-				}  //tant que $tab et $sort_lieu ont un nbre d'elts !=	
-//Boucle pour l'affichage html
-				foreach($sort_spec as $line){
-					
-					if($spectacle != $line[2]){
-						if($spectacle != "null"){
-							echo "</table>\n";
-						}
-						$spectacle = $line[2];
-						echo "<h2> " . $spectacle . "</h2>\n";
-						echo "<table>\n";
-					}
-					
-					echo "<tr>\n<td>";
-					echo "<Horaire> Le " . $line[0] . " " . " à " . $line[1] . "</Horaire>, " . "<titreSpectacle>". $line[2] . "</titreSpectacle> par <troupe>" . $line[5] . "</troupe><br/>\n";
-					echo "</td>\n <td>Reserver</td></tr>\n";
-				}
-					
-			
+						
 				
-				echo "<table>\n</div><!--class=\"decalage\"-->\n</main>\n";
-				fclose($handle);
-				
-			}
-		?>
+					
+					echo "</div><!--class=\"decalage\"-->\n</main>\n";
+					fclose($handle);
+					
+				}
+			?>
 	</body>
 </html>
