4b4dd5dd4024425ac6e41c06b6a120f59cb0d765
Azakira <47180416+Azakira@users.noreply.github.com>
2020-01-02 21:50:00 +0100

    modify csv method

--- visualisation.php
+++ visualisation.php
@@ -31,38 +31,22 @@
 		</div>
 		<main>
 			<div class="decalage">
-				<h1> <a href="Festival2018ProgrammationVueGlobale.php">Du 2 au 6 août 2019</a>
-			 	</h1>
-					<h2> <a href="spectacle.php">Une quinzaine de spectacles</a>, 
-					<a href="jours.php">une quarantaine de représentations</a>
-					</h2>
-					<h2>Des plus grands auteurs 
-					(<!--mettre lien--> Molière<!---->,					
-					<!--mettre autre lien-->Shakespeare
-					<!--</a>-->...) 
-					aux créations <a href="spectacle.php#labelSpectacle1">les plus originales	</a>,
-					</h2>
-				<h2> En plein air, les pieds dans l'herbe mais dans 
-					<a href="lieu.php">des cadres exceptionnels</a>, 
-				</h2>
-				<h2> <a href="tarif.php">en toute simplicité</a>, chaleur et convivialité, 
-				<a href="lieu.php">au cœur de la France</a>.
-				</h2>
-
-				<h1> <a href="InComingMaybeWhenWeWillHaveTimeToSpendOnIt.php">Changeons quelque chose à notre vie&#8239;!</a>
-				</h1>
-				<h2 class="adroite">
-					<a href="presentation.php">Théâtres de bourbon</a>
-				</h2>
-				<div class="adroite"> 
-					<a href="index.php">
-						<img class="petiteVignette" src="logo.jpg" alt="[logo de l'association vers l'accueil du site]"width=100% height=100% decoding=low>
-					</a>	
-				</div><!--  adroite -->
-
-				information de derniere minute: à cause du spectacle de son et lumiere de Moulins, la représentation de Tartuffe ou l'imposteur au CNCS de Moulins le 6 août 2019 initialement prévu à 20h30 est avancé à 20h00. 
-		
-				<iframe src="images/PdfFlyerA4RectoVerso.pdf" width="640" height="480"></iframe>
+				<?php 
+					/*CODE A METTRE DANS LE PANIER APRES CONFIRMATION DU PAYEMENT*/
+					if (($handle1 = fopen("ResultatsFestival.csv", "r")) !== FALSE) {
+						if (($handle2 = fopen("ResultatsFestival.csv", "w")) !== FALSE) {
+							fgetcsv($handle, 1000, ",");//On retire la 1ere ligne du csv (legendes)
+							while (($data = fgetcsv($handle1, 1000, ",")) !== FALSE) {
+								//on modifie les $data...
+								fputcsv($handle2, $data) //et on les remet dans le csv
+								
+							}
+							fclose($handle2);
+						}
+						fclose($handle1);
+					}
+				
+				?>
 			</div><!--  decalage -->	
 		</main> 
 	</body>
