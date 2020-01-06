3c45527d5e35a5947b0572477a5d82f7c2d50524
Azakira <47180416+Azakira@users.noreply.github.com>
2020-01-06 01:05:32 +0100

    Tableau fait

--- visualisation.php
+++ visualisation.php
@@ -22,7 +22,7 @@
 		<div class="menu">
 			<ul class="navbar">
 				<a href="index.php">Le site :</a>
-				<li>Qui sommes nous?</li>
+				<li>Qui sommes nous?</li>	
 				<li><a href="jours.php">Jour par Jour</a></li>
 				<li><a href="lieu.php">Lieu par Lieu</a></li>
 				<li><a href="spectacle.php">Spectacles</a></li>
@@ -33,19 +33,42 @@
 			<div class="decalage">
 				<?php 
 					/*CODE A METTRE DANS LE PANIER APRES CONFIRMATION DU PAYEMENT*/
-					if (($handle1 = fopen("ResultatsFestival.csv", "r")) !== FALSE) {
-						if (($handle2 = fopen("ResultatsFestival.csv", "w")) !== FALSE) {
+					if (($handle = fopen("ResultatsFestival2.csv", "r")) !== FALSE) { //r+ -> lecture et ecriture
 							fgetcsv($handle, 1000, ",");//On retire la 1ere ligne du csv (legendes)
-							while (($data = fgetcsv($handle1, 1000, ",")) !== FALSE) {
-								//on modifie les $data...
-								fputcsv($handle2, $data) //et on les remet dans le csv
-								
+							$tab = array();
+							while (($data = fgetcsv($handle, 1000, "\n")) !== FALSE) {
+								$line = array();				
+								foreach($data as $value){
+									$value = preg_replace_callback(
+										'/"(\\\\[\\\\"]|[^\\\\"])*"/',
+										function ($match){
+											$match = preg_replace("[,]", '&#44;', $match); //remplace les virgules par le symbole html
+											$match = preg_replace("[\"]", '', $match); //retire les guillemets
+											implode($match); //concatene le tout
+											return $match[0]; //probleme: cree un tableau dont la 1ere case contient ce que l'on veut :/
+										},
+										$value
+									);
+									
+									$value = preg_replace("[']", '&#146;', $value); //remplace les apostrophes par le symbole html
+									$fields = preg_split("[,]", $value);
+									array_push($tab, $fields);
+								}
+								//array_push($tab, $line);
+								//fputcsv($handle, $data); //et on les remet dans le csv
 							}
-							fclose($handle2);
+						fclose($handle);
+						echo "<p>";
+						foreach ($tab as $line){
+								foreach($line as $i => $val){
+									echo $val . " (" . $i . "); ";
+								}
+							echo "</p>\n";
 						}
-						fclose($handle1);
+						
 					}
-				
+					
+					
 				?>
 			</div><!--  decalage -->	
 		</main> 
